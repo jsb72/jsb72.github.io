04adc173376272147d57676425c442e8b0b74d0b
incon-science <1conscienceofficiel@gmail.com>
2026-04-03 18:46:59 +0200

    gfd

--- onlythevoid/index.service.worker.js
+++ onlythevoid/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775227263|2135678860';
+const CACHE_VERSION = '1775234805|23499401';
 /** @type {string} */
 const CACHE_PREFIX = 'Enjoy the Void-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
