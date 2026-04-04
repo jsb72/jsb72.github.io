282b8a2c6f423b6851e5c55a147720097ed2f1b6
incon-science <1conscienceofficiel@gmail.com>
2026-04-04 18:45:50 +0200

    fd

--- enjoythevoid/index.service.worker.js
+++ enjoythevoid/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775240046|3714967993';
+const CACHE_VERSION = '1775321134|78826277914';
 /** @type {string} */
 const CACHE_PREFIX = 'Enjoy the Void-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
