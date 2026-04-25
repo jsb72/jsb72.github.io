f46fa8051dd1b388352ab462696c9b3250823d71
incon-science <1conscienceofficiel@gmail.com>
2026-04-25 13:25:51 +0200

    fd

--- enjoythevoid_v272/index.service.worker.js
+++ enjoythevoid_v272/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777116102|10542581062';
+const CACHE_VERSION = '1777116342|10782915782';
 /** @type {string} */
 const CACHE_PREFIX = 'A False story-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
@@ -17,7 +17,7 @@ const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["index.wasm","index.pck","index.side.wasm","libropesim.web.template_release.wasm32.nothreads.wasm"];
+const CACHEABLE_FILES = ["index.wasm","index.pck","index.side.wasm","libropesim.web.template_release.wasm32.wasm"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
